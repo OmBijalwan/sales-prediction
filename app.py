147aeede66c414385ef8be9e60074cbfef49816e
Om Bijalwan <146260496+OmBijalwan@users.noreply.github.com>
2024-12-11 20:42:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,16 +25,21 @@ def result():
     X = np.array([[item_weight, item_fat_content, item_visibility, item_type, item_mrp, outlet_establishment_year,
                    outlet_size, outlet_location_type, outlet_type]])
 
-    scaler_path = r'C:\Users\ACER\Desktop\python\models\sc.sav'
+    #scaler_path = r'C:\Users\ACER\Desktop\python\models\sc.sav'
 
     #scaler_path = r'E:\python\models\sc.sav'
+
+    scaler_path = './models/sc.sav'
+
     sc = joblib.load(scaler_path)
 
     X_std = sc.transform(X)
 
-    model_path = r'C:\Users\ACER\Desktop\python\models\rf.sav'
+    # model_path = r'C:\Users\ACER\Desktop\python\models\rf.sav'
 
     # model_path = r'E:\python\models\rf.sav'
+
+    model_path = './models/rf.sav'
 
     model = joblib.load(model_path)
 
