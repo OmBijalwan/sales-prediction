0f0e8ee60f1aa6627193d0d884020feee5f534f1
Om Bijalwan <146260496+OmBijalwan@users.noreply.github.com>
2024-12-12 00:05:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,7 @@ def result():
     return render_template('result.html', prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=9457)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
